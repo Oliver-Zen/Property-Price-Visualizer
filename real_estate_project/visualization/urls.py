f5645734af370740ced8c3b84bed3b3466077f70
Bai, Linhao <lbai46@gatech.edu>
2024-11-23 01:28:30 -0500

    update

--- real_estate_project/visualization/urls.py
+++ real_estate_project/visualization/urls.py
@@ -5,7 +5,13 @@ from . import views
 
 urlpatterns = [
     path('', views.visualization_view, name='visualization'),
+<<<<<<< HEAD
+    path('visualization/', views.visualization_view, name='visualization'),
+    path('choropleth/', views.choropleth_view, name='choropleth'),
+    path('price_query/', views.price_query_view, name='price_query'),
+=======
     path('choropleth/', views.choropleth_view, name='choropleth'),
     path('data/', views.get_data, name='get_data'),
     path('price-query/', views.price_query_view, name='price_query'),
+>>>>>>> eab1542 (integrate django)
 ]
